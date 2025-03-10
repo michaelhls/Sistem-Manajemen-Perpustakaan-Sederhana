dd146d1b236ed3124bc44bab3232b72eb29f6818
Michael H <90170582+michaelhls@users.noreply.github.com>
2025-03-10 20:59:17 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+class User {
+    public void interact() {
+    }
+}
+
+class Admin extends User {
+    @Override
+    public void interact() {
+        System.out.println("");
+    }
+}
+
+class Member extends User {
+    @Override
+    public void interact() {
+        System.out.println("");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        User admin = new Admin();
+        User member = new Member();
+
+        admin.interact();
+        member.interact();
+    }
+}
